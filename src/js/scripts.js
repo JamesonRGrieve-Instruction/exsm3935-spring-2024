24ba93c26a35b03eec9966bc25111f7e2bfa22c2
James Grieve <jgrieve@ualberta.ca>
2024-05-22 20:31:20 -0600

    Output line breaks;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,10 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   // This is where the code you're actually experimenting with goes.
-  output((await input("Please enter some text: ")).toLowerCase().replace("hello", "goodbye").toUpperCase());  
+  output("-----\nTest\n-----");
+  let textInput = (await input("Enter something:")).toLowerCase().replace("hello", "goodbye").toUpperCase();
+
+  output(textInput);  
 }
 
 
