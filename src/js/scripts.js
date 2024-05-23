da36aa4e95851fc3b1aa0760f295ec5dd2ff91db
James Grieve <jgrieve@ualberta.ca>
2024-05-22 19:44:44 -0600

    Basic arithmetic;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,12 +3,12 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
-  }
+  let leftOperand = await input("Please enter a number: ");
+  let rightOperand = await input("Please enter another number: ");
+  // Because we don't know decisions yet, entering something that isn't a number will cause the result to be NaN (Not a Number).
+  let sum = Number(leftOperand) + Number(rightOperand);
+  output(sum);
 }
+
+
+
