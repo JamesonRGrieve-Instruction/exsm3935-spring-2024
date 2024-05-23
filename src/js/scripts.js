0948d59f62270de3e256cf6a9e495267d9a57e13
James Grieve <jgrieve@ualberta.ca>
2024-05-22 20:07:36 -0600

    Floating point errors;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,9 +6,14 @@ async function main() {
   let leftOperand = await input("Please enter a number: ");
   let rightOperand = await input("Please enter another number: ");
   // Because we don't know decisions yet, entering something that isn't a number will cause the result to be NaN (Not a Number).
-  let sum = Number(leftOperand) + Number(rightOperand);
+  let sum = (Number(leftOperand)*100 + Number(rightOperand)*100)/100;
   output(sum);
 }
 
+// Computers are subject to "floating point errors" when dealing with decimal numbers.
+// This is because computers store numbers in binary, and some numbers that are easy to represent in decimal are difficult to represent in binary.
+// Something like how we have a difficult time representing some fractions in decimal:
+// 1/3 + 2/3 = 3/3
+// 0.3333333333333333 + 0.6666666666666666 = 0.9999999999999999
 
 
