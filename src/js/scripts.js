8c3d69877127b671229a41fd5b6cf284ff6874c9
James Grieve <jgrieve@ualberta.ca>
2024-05-23 19:27:01 -0600

    Integer division and remainders;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,8 +5,9 @@ async function main() {
   // This is where the code you're actually experimenting with goes.
   const firstOperand = await input("Enter the first number: ");
   const secondOperand = await input("Enter the second number: ");
-  const result = firstOperand / secondOperand;
-  output(result);  
+  const quotient = Math.floor(firstOperand / secondOperand);
+  const remainder = firstOperand % secondOperand;
+  output(`${quotient}R${remainder}`);  
 }
 
 
