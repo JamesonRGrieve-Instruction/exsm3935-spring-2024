858dc5158ad7fdddb4b1b6ad100b5a9b07f96be6
James Grieve <jgrieve@ualberta.ca>
2024-05-23 19:22:09 -0600

    Implicit conversions (vs with concatenation);

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,10 +3,10 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   // This is where the code you're actually experimenting with goes.
-  output("-----\nTest\n-----");
-  let textInput = (await input("Enter something:")).toLowerCase().replace("hello", "goodbye").toUpperCase();
-
-  output(textInput);  
+  let firstOperand = await input("Enter the first number: ");
+  let secondOperand = await input("Enter the second number: ");
+  let result = firstOperand * secondOperand;
+  output(result);  
 }
 
 
