7832d949b875fcaaf1806793bd53acd38f603cf4
James Grieve <jgrieve@ualberta.ca>
2024-05-30 19:29:34 -0600

    Simplify by removing else;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,13 +3,11 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   const userInput = await input("Please enter a string: ");
-  let spaceNumber;
+  let spaceNumber = 0;
   if (userInput.includes(" ")) {
     // If we use the let keyword inside a code block, we cannot reference the variable we have declared outside of the enclosing code block.
     // Therefore, we must declare the variable outside of the code block, and assign the value herein.
     spaceNumber = userInput.split(" ").length - 1;
-  } else {
-    spaceNumber = 0;
   }
   output("The number of spaces in the string is: " + spaceNumber);
 }
