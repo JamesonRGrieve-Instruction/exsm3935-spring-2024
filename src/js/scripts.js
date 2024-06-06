d5483ed537dbe038d13d7afaeb3ab285bc369a35
James Grieve <jgrieve@ualberta.ca>
2024-06-05 19:11:54 -0600

    Throw exception; Try/catch;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,12 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  const userInput = await input("Please enter a string: ");
-  let spaceNumber = 0;
-  if (userInput.includes(" ")) {
-    // If we use the let keyword inside a code block, we cannot reference the variable we have declared outside of the enclosing code block.
-    // Therefore, we must declare the variable outside of the code block, and assign the value herein.
-    spaceNumber = userInput.split(" ").length - 1;
-    // "Hello, World!" --> ["Hello,", "World!"] --> 1 space
+  try {
+    const userInput = await input("Please enter a string: ");
+    if (userInput.trim() === "") {
+      throw new Error("A value was not provided to userInput.");
+    }
+  } catch (error) {
+    output(error);
   }
-  output("The number of spaces in the string is: " + spaceNumber);
 }
