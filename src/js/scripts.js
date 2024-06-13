4625eedbc2a67c9f56e94a4c203c6922bdac9223
James Grieve <jgrieve@ualberta.ca>
2024-06-12 20:09:16 -0600

    TicTacToe board;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,44 +2,11 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  const myDictionary = {
-    James: "Grieve",
-    Golden: "Delicious",
-    Granny: "Smith",
-    Red: "Delicious",
-    "Cox's": "Orange",
-    Fuji: "Apple",
-  };
+  const ticTacToe = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
 
-  output(myDictionary["James"]);
-  output(myDictionary.James);
-
-  output(myDictionary["Cox's"]);
-
-  myDictionary["Test"] = "Test123";
-  output(myDictionary.Test);
-
-  myDictionary.Test = "Test456";
-  output(myDictionary.Test);
-
-  delete myDictionary.Test;
-  output(myDictionary.Test);
-
-  myDictionary.James = undefined;
-  output(myDictionary.James);
-
-  output(Object.keys(myDictionary));
-  output(Object.values(myDictionary));
-
-  delete myDictionary.James;
-
-  output(Object.keys(myDictionary));
-  output(Object.values(myDictionary));
-
-  output(Object.entries(myDictionary));
-
-  const myArray = [1, 2, 3, 4];
-  myArray[9] = 10;
-  output(myArray);
-  output(myArray[20]);
+  output(ticTacToe[0][1]);
 }
