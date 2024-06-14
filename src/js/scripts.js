3fe9dbdb7f377fcd52a419c9c3b3095e52a52bcc
James Grieve <jgrieve@ualberta.ca>
2024-06-13 19:08:38 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,29 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-  let leftOperand = await input("Please enter a number: ");
-  let rightOperand = await input("Please enter another number: ");
-  // Because we don't know decisions yet, entering something that isn't a number will cause the result to be NaN (Not a Number).
-  let sum = Number(leftOperand) + Number(rightOperand);
-  output(sum);
+  const orders = new Set(["ABC123", "DEF456", "GHI789"]);
+  const orderQtys = [5, 42, 12];
+  try {
+    const orderNumber = (await input("Please enter your order number: ")).trim();
+    if (orderNumber.length !== 6) {
+      throw new Error("Invalid order number - must be 6 characters.");
+    }
+    if (!orderNumber.match(/^[a-zA-Z0-9]+$/)) {
+      throw new Error("Invalid order number - must be alphanumeric.");
+    }
+    if (orders.has(orderNumber)) {
+      throw new Error("Order already exists.");
+    }
+    orders.add(orderNumber);
+    const orderQty = (await input("Please enter the quantity: ")).trim();
+    if (isNaN(orderQty)) {
+      throw new Error("Invalid quantity - must be a number.");
+    }
+    if (orderQty < 1 || orderQty > 100) {
+      throw new Error("Invalid quantity - must be between 1 and 100 inclusive.");
+    }
+    orderQtys.push(orderQty);
+  } catch (error) {
+    output(error.message);
+  }
 }
-
-
-
