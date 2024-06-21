6528dcd9199da0c545bbae7dace3d7ac801a9887
James Grieve <jgrieve@ualberta.ca>
2024-06-20 20:08:48 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,26 +2,51 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  try {
-    const username = (await input("Please enter your username: ")).trim();
-    if (username === "") {
-      throw new Error("Username cannot be empty.");
+  let userMenuSelection;
+  const users = {};
+  let loggedIn = false;
+  do {
+    output("Welcome to the login page.\n1. Register\n2. Login\n3. Exit");
+    userMenuSelection = (await input("Please make a selection: ")).trim();
+    if (userMenuSelection === "1") {
+      const username = (await input("Please enter your username: ")).trim();
+      if (Object.keys(users).includes(username)) {
+        output("Username already exists.");
+      } else if (username === "") {
+        output("Username cannot be empty.");
+      } else {
+        const password = (await input("Please enter your password: ")).trim();
+        if (password === "") {
+          output("Password cannot be empty.");
+        } else if (password.length < 8) {
+          output("Password is too short.");
+        } else {
+          users[username] = password;
+          output("Registration successful.");
+        }
+      }
+    } else if (userMenuSelection === "2") {
+      const username = (await input("Please enter your username: ")).trim();
+      if (username === "") {
+        output("Username cannot be empty.");
+      } else if (!Object.keys(users).includes(username)) {
+        output("Username does not exist.");
+      } else {
+        const password = (await input("Please enter your password: ")).trim();
+        if (password === "") {
+          output("Password cannot be empty.");
+        } else if (password.length < 8) {
+          output("Password is too short.");
+        } else if (users[username] !== password) {
+          output("Password is incorrect.");
+        } else {
+          loggedIn = true;
+          output("Login successful, welcome " + username + ".");
+        }
+      }
+    } else if (userMenuSelection !== "3") {
+      output("Invalid selection.");
     }
-    if (username !== "admin") {
-      throw new Error("Username does not exist.");
-    }
-    const password = (await input("Please enter your password: ")).trim();
-    if (password === "") {
-      throw new Error("Password cannot be empty.");
-    }
-    if (password.length < 8) {
-      throw new Error("Password is too short.");
-    }
-    if (password !== "adminpwd") {
-      throw new Error("Password is incorrect.");
-    }
-    output("Login successful.");
-  } catch (error) {
-    output(error);
-  }
+  } while (userMenuSelection !== "3" && !loggedIn);
+  output("Goodbye.");
 }
