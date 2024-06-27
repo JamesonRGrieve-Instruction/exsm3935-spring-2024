dbb05a68da4529b30c8a5f2d0f282c6e86eb6b08
James Grieve <jgrieve@ualberta.ca>
2024-06-26 19:42:59 -0600

    Validation function with multiple parameters;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,38 @@
 // eslint-disable-next-line no-unused-vars
 /* global output, input */
 
-// Our declaration does not run the code when it is declared - it only runs when called.
-// Declarations are in the syntax of:
-// function functionName(parameterOne, parameterTwo) { code }
-// See below for the definition of parameters.
-function printMessage(aString) {
-  // Parameters (aString) represent the inputs to the function.
-  // Think of them like the receiving bay of a factory.
-  // You can have zero, one, or many parameters - they are separated by commas, and mapped positionally.
-  // Parameter functions as constants into which arguments (values) are injected for the lifespan
-  // of the function. They can technically be reassigned, but it is typically bad practice.
-  // let aString = "Hello, World!";
-
-  output(aString);
-  // Output here functions the same way as every other script we've written,
-  // it outputs a string message to the console environment.
-
-  return aString;
-  // Think of return like the shipping bay of a factory.
-  // Whatever is provided to the return keyword will be exported or
-  // "returned" to where the function was called.
+function validateInt(value, min, max) {
+  if (!Number.isInteger(Number(value))) {
+    throw new Error("Value must be a number");
+  }
+  if (!Number.isInteger(Number(min))) {
+    throw new Error("Minimum must be a number");
+  }
+  if (!Number.isInteger(Number(max))) {
+    throw new Error("Maximum must be a number");
+  }
+  let toReturn;
+  if (value < min || value > max) {
+    toReturn = false;
+  } else {
+    toReturn = true;
+  }
+  // If you return a list of values outside of an array, only the last value will actually be returned.
+  // If you want to return multiple values, you must return them in an array (or dictionary/object).
+  //return value, min, max, toReturn;
+  return toReturn;
 }
 
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // Calling a function is done using the syntax:
-  // functionName(argumentOne, argumentTwo);
-  // You should have the same number of arguments as you have parameters, in the same order.
-  const myConstant = printMessage("Hello, World!");
-  printMessage("Hello, Again!");
-  // If the function returns something, it can be retrieved for use in an expression, stored in
-  // a variable, etc. In this case the return value takes the place of the function call
-  // and is stored in the myVariable constant.
-
-  output(myConstant);
-  // Finally, we can output the return value of the function to the console.
-  // This is the second instance you will see on the console, as when we call printMessage,
-  // it will run and also includes an output().
+  const userInput = await input("Enter a number between 1 and 10: ");
+  try {
+    if (validateInt(userInput, 1, 10)) {
+      output("You entered a valid number");
+    } else {
+      output("You entered an invalid number");
+    }
+  } catch (error) {
+    output(error);
+  }
 }
