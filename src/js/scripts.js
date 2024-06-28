75800bdab9fb88620dda8fbd07810147d4890e8c
James Grieve <jgrieve@ualberta.ca>
2024-06-27 19:25:26 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,36 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-  let leftOperand = await input("Please enter a number: ");
-  let rightOperand = await input("Please enter another number: ");
-  // Because we don't know decisions yet, entering something that isn't a number will cause the result to be NaN (Not a Number).
-  let sum = Number(leftOperand) + Number(rightOperand);
-  output(sum);
+  let userInput;
+  const highscores = {};
+  do {
+    output("High/Low Game\na. Play Game\nb. Highscores\nc. Quit");
+    userInput = (await input("Choice: ")).trim().toLowerCase();
+    if (userInput === "a") {
+      let guesses = 0;
+      const target = Math.floor(Math.random() * 100) + 1;
+      let guess;
+      do {
+        guess = parseInt(await input("Enter your guess: "), 10);
+        guesses += 1;
+        if (guess < target) {
+          output("Higher");
+        } else if (guess > target) {
+          output("Lower");
+        }
+      } while (guess !== target);
+      output(`You guessed it in ${guesses} tries!`);
+      let name;
+      do {
+        name = (await input("Enter your name: ")).trim();
+      } while (!name.includes(" "));
+      highscores[name] = guesses;
+    } else if (userInput === "b") {
+      for (const key of Object.keys(highscores)) {
+        output(`${key}: ${highscores[key]}`);
+      }
+    } else if (userInput !== "c") {
+      output("Invalid selection. Please try again.");
+    }
+  } while (userInput !== "c");
 }
-
-
-
